d5515707773b08314bb84c671332a768cb92d772
Inho Oh <inho.oh@sk.com>
2022-09-16 13:55:55 +0900

    libnugu-plugins 1.7.3 (new formula)
    add libnugu-plugins 1.7.3
    
    Closes #2.
    
    Signed-off-by: Inho Oh <inho.oh@sk.com>
    Signed-off-by: Inho Oh <webispy@gmail.com>

---
+++ Formula/libnugu-plugins.rb
@@ -0,0 +1,35 @@
+class LibnuguPlugins < Formula
+  desc "Default Plugins for NUGU SDK"
+  homepage "https://github.com/nugu-developers/nugu-linux"
+  url "https://github.com/nugu-developers/nugu-linux/archive/a5bffd0.tar.gz"
+  version "1.7.3"
+  sha256 "1d7846706201448156b9a6ba38535dcae6b785cc96ccd102f6da40b91052065b"
+  license "Apache-2.0"
+
+  # brew install --build-from-source --HEAD libnugu
+  head "https://github.com/nugu-developers/nugu-linux.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gst-plugins-bad"
+  depends_on "gst-plugins-good"
+  depends_on "gstreamer"
+  depends_on "libnugu"
+  depends_on "gst-plugins-ugly" => :recommended
+
+  def install
+    args = %w[
+      -DENABLE_VENDOR_LIBRARY=OFF
+      -DENABLE_PLUGINS_ONLY=ON
+      -DPACKAGING=ON
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "true"
+  end
+end
