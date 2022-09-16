709e0122987fc0534bf683af87d43bd7cb97ac92
Inho Oh <inho.oh@sk.com>
2022-09-16 13:29:55 +0900

    libnugu 1.7.3 (new formula)
    add libnugu 1.7.3
    
    Closes #1.
    
    Signed-off-by: Inho Oh <inho.oh@sk.com>
    Signed-off-by: Inho Oh <webispy@gmail.com>

---
+++ Formula/libnugu.rb
@@ -0,0 +1,37 @@
+class Libnugu < Formula
+  desc "C/C++ library for NUGU SDK"
+  homepage "https://github.com/nugu-developers/nugu-linux"
+  url "https://github.com/nugu-developers/nugu-linux/archive/a5bffd0.tar.gz"
+  version "1.7.3"
+  sha256 "1d7846706201448156b9a6ba38535dcae6b785cc96ccd102f6da40b91052065b"
+  license "Apache-2.0"
+
+  # brew install --build-from-source --HEAD libnugu
+  head "https://github.com/nugu-developers/nugu-linux.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "curl"
+  depends_on "glib"
+  depends_on "jsoncpp"
+  depends_on "openssl"
+
+  def install
+    args = %W[
+      -DENABLE_VENDOR_LIBRARY=OFF
+      -DENABLE_LIBRARY_ONLY=ON
+      -DENABLE_BUILTIN_JSONCPP=OFF
+      -DENABLE_BUILTIN_CURL=OFF
+      -DPLUGIN_DIR=#{HOMEBREW_PREFIX}/lib/nugu
+      -DPACKAGING=ON
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "true"
+  end
+end
