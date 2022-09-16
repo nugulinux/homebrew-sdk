20bd6c0f247e6a3de7fc06160752cda4b745dff3
Inho Oh <inho.oh@sk.com>
2022-09-16 14:28:59 +0900

    add libnugu-examples 1.7.3
    Signed-off-by: Inho Oh <inho.oh@sk.com>

---
+++ Formula/libnugu-examples.rb
@@ -0,0 +1,31 @@
+class LibnuguExamples < Formula
+  desc "Example applications for NUGU SDK"
+  homepage "https://github.com/nugu-developers/nugu-linux"
+  url "https://github.com/nugu-developers/nugu-linux/archive/a5bffd0.tar.gz"
+  version "1.7.3"
+  sha256 "1d7846706201448156b9a6ba38535dcae6b785cc96ccd102f6da40b91052065b"
+  license "Apache-2.0"
+
+  # brew install --build-from-source --HEAD libnugu
+  head "https://github.com/nugu-developers/nugu-linux.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libnugu"
+  depends_on "libnugu-plugins"
+
+  def install
+    args = %w[
+      -DENABLE_EXAMPLES_ONLY=ON
+      -DPACKAGING=ON
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "true"
+  end
+end
