df51f4769c8308306098cc8877e6ac4995612fa1
Inho Oh <webispy@gmail.com>
2022-09-29 17:36:34 +0900

    update libnugu
    Signed-off-by: Inho Oh <webispy@gmail.com>

--- Formula/libnugu.rb
+++ Formula/libnugu.rb
@@ -1,9 +1,9 @@
 class Libnugu < Formula
   desc "C/C++ library for NUGU SDK"
   homepage "https://github.com/nugu-developers/nugu-linux"
-  url "https://github.com/nugu-developers/nugu-linux/archive/a5bffd0.tar.gz"
+  url "https://github.com/nugu-developers/nugu-linux/archive/742ca26.tar.gz"
   version "1.7.3"
-  sha256 "1d7846706201448156b9a6ba38535dcae6b785cc96ccd102f6da40b91052065b"
+  sha256 "b5d74620bd33f27118e3c30a5c27838a3b43e434b8640b58c6fef4674f6e6cd7"
   license "Apache-2.0"
 
   # brew install --build-from-source --HEAD libnugu
@@ -20,14 +20,16 @@ class Libnugu < Formula
   depends_on "curl"
   depends_on "glib"
   depends_on "jsoncpp"
+  depends_on "libnugu-epd"
+  depends_on "libnugu-kwd"
   depends_on "openssl"
 
   def install
     args = %W[
-      -DENABLE_VENDOR_LIBRARY=OFF
       -DENABLE_LIBRARY_ONLY=ON
       -DENABLE_BUILTIN_JSONCPP=OFF
       -DENABLE_BUILTIN_CURL=OFF
+      -DENABLE_GSTREAMER_PLUGIN=OFF
       -DPLUGIN_DIR=#{HOMEBREW_PREFIX}/lib/nugu
       -DPACKAGING=ON
     ]
