b3516d64a29952f516ab380de5dfc844d9647ce0
Inho Oh <webispy@gmail.com>
2022-09-29 16:59:30 +0900

    add libnugu-kwd
    Signed-off-by: Inho Oh <webispy@gmail.com>

---
+++ Formula/libnugu-kwd.rb
@@ -0,0 +1,19 @@
+class LibnuguKwd < Formula
+  desc "C/C++ library for NUGU SDK Keyword detector"
+  homepage "https://github.com/nugulinux/nugu-kwd"
+  url "https://github.com/nugulinux/nugu-kwd/releases/download/0.9.38ubuntu6/libnugu-kwd-src.tgz"
+  sha256 "ad524e18328dde5f07381cfad56a62a03f11108db3696a7d1f72e7996e8257aa"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "true"
+  end
+end
