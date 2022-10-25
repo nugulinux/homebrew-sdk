cfe10479404b6054ebb80aa3b18c7e641d352b2d
Inho Oh <webispy@gmail.com>
2022-10-25 19:26:31 +0900

    rebuild libnugu-epd
    Signed-off-by: Inho Oh <webispy@gmail.com>

--- Formula/libnugu-epd.rb
+++ Formula/libnugu-epd.rb
@@ -5,12 +5,6 @@ class LibnuguEpd < Formula
   sha256 "511ec9b5d08c55689eee7ee72412fb4cb29b87c644edeaf8b88f30cb48298af9"
   license "Apache-2.0"
 
-  bottle do
-    root_url "https://github.com/nugulinux/homebrew-sdk/releases/download/libnugu-epd-2.1.11"
-    sha256 cellar: :any, monterey: "3143873069c44ed5bbe8a6dc8e96b284f65b70b75fdd0e11af11efcf275cc872"
-    sha256 cellar: :any, big_sur:  "ff45d9e4fec9549de3c29298ccbb8687ca7620463c4686a73bd6b04477e9bd90"
-  end
-
   depends_on "cmake" => :build
 
   def install
