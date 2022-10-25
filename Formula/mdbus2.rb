86a8f9507bc1b97d308d83a7b68ac06718514bde
Inho Oh <webispy@gmail.com>
2022-10-25 13:09:26 +0000

    mdbus2: add 2.3.3 bottle.

--- Formula/mdbus2.rb
+++ Formula/mdbus2.rb
@@ -9,6 +9,14 @@ class Mdbus2 < Formula
   # brew install --build-from-source --HEAD mdbus2
   head "https://github.com/webispy/mdbus.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/nugulinux/homebrew-sdk/releases/download/mdbus2-2.3.3"
+    rebuild 1
+    sha256 cellar: :any, arm64_monterey: "9546c9f69cea7bbe3c2ca5cd0c5b997e3652960efe456c14d2e67b7aae06d903"
+    sha256 cellar: :any, monterey:       "0cb66bb9cfffd20808f4713d965a2cdcc8a7b707fb6b97c1eef93eb4daac34dd"
+    sha256 cellar: :any, big_sur:        "46118759065a666aa476e82b20f86d1da7b69f547a1c444c8adfba062563521f"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
