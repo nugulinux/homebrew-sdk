fcd3e61ab21e26722fe7151f6eb5ac942df0a563
Inho Oh <webispy@gmail.com>
2022-10-25 21:51:35 +0900

    rebuild mdbus2
    Signed-off-by: Inho Oh <webispy@gmail.com>

--- Formula/mdbus2.rb
+++ Formula/mdbus2.rb
@@ -9,12 +9,6 @@ class Mdbus2 < Formula
   # brew install --build-from-source --HEAD mdbus2
   head "https://github.com/webispy/mdbus.git", branch: "master"
 
-  bottle do
-    root_url "https://github.com/nugulinux/homebrew-sdk/releases/download/mdbus2-2.3.3"
-    sha256 cellar: :any, monterey: "31ce02cbff892721fe89275395c7aee4bf25b2ed8e60a6365cd4965f23b9b0a7"
-    sha256 cellar: :any, big_sur:  "ae1f85e15747d2ef283f91740f5ac3382545177ac434e1efb1e12248cf35d2ac"
-  end
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
