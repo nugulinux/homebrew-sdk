874bb95a79017c1168e3fd0344cc1006a3c9bbc2
Inho Oh <webispy@gmail.com>
2022-10-25 12:36:33 +0000

    libnugu-extension: add 1.7.3 bottle.

--- Formula/libnugu-extension.rb
+++ Formula/libnugu-extension.rb
@@ -9,6 +9,14 @@ class LibnuguExtension < Formula
   sha256 "3078091c659fb5d9f2b672b4f6564e64f3faa3f14803cd26e5e1dfcb27803ca5"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/nugulinux/homebrew-sdk/releases/download/libnugu-extension-1.7.3"
+    rebuild 2
+    sha256 arm64_monterey: "1654a92f7be7efb0cd2e15573dfe0c5b8f895f0abbcd1f7d645bde809f22835b"
+    sha256 monterey:       "ddf1754fccfd93626e631563b13aaaa666fd33e636f8ff78716b1c69a262d395"
+    sha256 big_sur:        "0b9aef2501bb6887c202e1c547a78b86fd7625c43bdc6113fdbc7df089475cce"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "libnugu"
