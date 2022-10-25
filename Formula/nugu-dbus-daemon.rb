f42188c51c1a9d1c898240a0cf4532f9f3bac620
Inho Oh <webispy@gmail.com>
2022-10-25 22:00:56 +0900

    rebuild nugu-dbus-daemon
    Signed-off-by: Inho Oh <webispy@gmail.com>

--- Formula/nugu-dbus-daemon.rb
+++ Formula/nugu-dbus-daemon.rb
@@ -6,12 +6,6 @@ class NuguDbusDaemon < Formula
   sha256 "9bd7b933a4c1ea2b906362dfd47091640740345ddbed3c5528a38ed0117ef06c"
   license "Apache-2.0"
 
-  bottle do
-    root_url "https://github.com/nugulinux/homebrew-sdk/releases/download/nugu-dbus-daemon-1.7.3"
-    sha256 cellar: :any_skip_relocation, monterey: "741fe02d22cf1bf79923c617d5334290979c5c080705648960296535beb004f0"
-    sha256 cellar: :any_skip_relocation, big_sur:  "fda23d78067f4decc277f31411b01fa4f030df566b023058e75408f7e8e82193"
-  end
-
   depends_on "cmake" => :build
   depends_on "dbus"
 
