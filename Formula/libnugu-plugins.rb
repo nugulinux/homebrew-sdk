9d61fd2d88a33c4d4d1c0468d99b5539ca4b18f3
LordVader <14344535+kimhyungrok@users.noreply.github.com>
2022-12-21 08:44:04 +0000

    libnugu-plugins: update 1.7.4 bottle.

--- Formula/libnugu-plugins.rb
+++ Formula/libnugu-plugins.rb
@@ -10,11 +10,10 @@ class LibnuguPlugins < Formula
   head "https://github.com/nugu-developers/nugu-linux.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/nugulinux/homebrew-sdk/releases/download/libnugu-plugins-1.7.3"
-    rebuild 2
-    sha256 cellar: :any, arm64_monterey: "4b9b294222215de9d04090c22726b88a4d58ed2c96862646df21d228ced385d6"
-    sha256 cellar: :any, monterey:       "96a91d7e1e42b003e3236b6354f5fc2c0b9dc756faea57d8fca82bd3b17fb345"
-    sha256 cellar: :any, big_sur:        "32ed4141969d0c1bcc41f2217e7d6bcc2eb4aaad2938e30cc22556a1aafe2354"
+    root_url "https://github.com/nugulinux/homebrew-sdk/releases/download/libnugu-plugins-1.7.4"
+    sha256 cellar: :any, arm64_monterey: "c0353739de0973ae7f0ecb987023564cf64548ad7320479596ef1a50164370ca"
+    sha256 cellar: :any, monterey:       "45e760372a2e8442845a84e031884b86c1a550a73908856ebda127698b0057e2"
+    sha256 cellar: :any, big_sur:        "3f6ddfba596880d92b503954203d459cf83799c0e5506aa61b444d3902761298"
   end
 
   depends_on "cmake" => :build
