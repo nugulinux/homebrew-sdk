7233639caed344d5ad42d4dd7bd2a719ba1abed6
LordVader <14344535+kimhyungrok@users.noreply.github.com>
2022-12-21 07:42:21 +0000

    libnugu-kwd: update 0.9.38 bottle.

--- Formula/libnugu-kwd.rb
+++ Formula/libnugu-kwd.rb
@@ -7,10 +7,10 @@ class LibnuguKwd < Formula
 
   bottle do
     root_url "https://github.com/nugulinux/homebrew-sdk/releases/download/libnugu-kwd-0.9.38"
-    rebuild 1
-    sha256 cellar: :any, arm64_monterey: "79b09bc6d80a6c46d7918c0b159cddbc8bd4fef26c7eb1ee93fc4feeff1e6708"
-    sha256 cellar: :any, monterey:       "9185425e8b3d1cef7ae58611875023bd28991bcf316458d90b29429a091af00d"
-    sha256 cellar: :any, big_sur:        "c2efc523780b121cb602f3451a9d84a24a69216a3cf679de29388b41bc7e9a3c"
+    rebuild 2
+    sha256 cellar: :any, arm64_monterey: "3509bc5b7f2ba8dbe71ef3a3293b4430d59577b958190871daea4e5d3629ff1d"
+    sha256 cellar: :any, monterey:       "445540b9abb048215b6d68c8ef6d6e92aab0b32f202c76ae0fefc84ef6ddacaa"
+    sha256 cellar: :any, big_sur:        "c7742ebd95888852ab5a1e1ef0bf4380f4a9cd0ccc8c274c0446aa4f4694663f"
   end
 
   depends_on "cmake" => :build
