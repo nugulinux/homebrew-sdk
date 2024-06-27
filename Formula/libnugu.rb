f664c6bd64908f74024d83c57a105e69f37202eb
Inho Oh <webispy@gmail.com>
2024-06-27 04:08:11 +0000

    libnugu: update 1.7.6 bottle.

--- Formula/libnugu.rb
+++ Formula/libnugu.rb
@@ -11,6 +11,9 @@ class Libnugu < Formula
 
   bottle do
     root_url "https://github.com/nugulinux/homebrew-sdk/releases/download/libnugu-1.7.6"
+    sha256 arm64_sonoma: "3387358163596ceb62471bd30cc2df079cf3ecf68f8fd75e88df068275fa13d9"
+    sha256 ventura:      "afb89a15a6d92857ca5336d898d9cf3fe27a764ff0963786073483fc481ea716"
+    sha256 monterey:     "745bff1f492f482bb2d3e4450942ea8f1a4af5a1323af6574ab59916ed90a2c8"
   end
 
   depends_on "cmake" => :build
