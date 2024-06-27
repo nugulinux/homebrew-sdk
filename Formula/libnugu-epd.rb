fc1e07bbbacd86da6faddbeb2989f2db68cf6c00
Inho Oh <webispy@gmail.com>
2024-06-27 02:38:41 +0000

    libnugu-epd: update 2.1.11 bottle.

--- Formula/libnugu-epd.rb
+++ Formula/libnugu-epd.rb
@@ -7,6 +7,9 @@ class LibnuguEpd < Formula
 
   bottle do
     root_url "https://github.com/nugulinux/homebrew-sdk/releases/download/libnugu-epd-2.1.11"
+    sha256 cellar: :any, arm64_sonoma: "0f5de6435d421e20fc2c3364a9050a566a169d09a99788ad48202c551b37ba20"
+    sha256 cellar: :any, ventura:      "e85f247e6e45b3abf353a3fd33a34c4a8c2546d5a025db014ff79cdba8d8f80d"
+    sha256 cellar: :any, monterey:     "46bf9fa9d0d1c6b2d8c8c28059689cd72c766927c2b017b2bbf3cea482a7c121"
   end
 
   depends_on "cmake" => :build
