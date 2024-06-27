6b046543375dda17e163a2a9cc9fd2469144566d
Inho Oh <webispy@gmail.com>
2024-06-27 02:30:50 +0000

    libnugu-kwd: update 0.9.38 bottle.

--- Formula/libnugu-kwd.rb
+++ Formula/libnugu-kwd.rb
@@ -7,6 +7,9 @@ class LibnuguKwd < Formula
 
   bottle do
     root_url "https://github.com/nugulinux/homebrew-sdk/releases/download/libnugu-kwd-0.9.38"
+    sha256 cellar: :any, arm64_sonoma: "4c9a0610d2e4d9d88b1cd0c3a1c33006376a1e700ddc6ce51a5c84199454d2f9"
+    sha256 cellar: :any, ventura:      "fbb40b8dcd4236a1955bd6dd3be4109086cc57f1583764ef23ce60ba2e38a85b"
+    sha256 cellar: :any, monterey:     "ef6e608706191d85d7fefabdfb21e7da421c246b618a2fe5453af2252b493be7"
   end
 
   depends_on "cmake" => :build
