bab9409c2ecd9c847a8c2c2f18d9e625e7eb5431
Inho Oh <webispy@gmail.com>
2024-06-27 11:02:54 +0900

    libnugu-kwd: update
    Closes #36.
    
    Signed-off-by: Inho Oh <webispy@gmail.com>

---
+++ Formula/libnugu-kwd.rb
@@ -0,0 +1,23 @@
+class LibnuguKwd < Formula
+  desc "C/C++ library for NUGU SDK Keyword detector"
+  homepage "https://github.com/nugulinux/nugu-kwd"
+  url "https://github.com/nugulinux/nugu-kwd/releases/download/0.9.38ubuntu6/libnugu-kwd-src.tgz"
+  sha256 "ad524e18328dde5f07381cfad56a62a03f11108db3696a7d1f72e7996e8257aa"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/nugulinux/homebrew-sdk/releases/download/libnugu-kwd-0.9.38"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "true"
+  end
+end
